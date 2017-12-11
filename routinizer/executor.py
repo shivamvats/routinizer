6441317e7a538b5b9d27b2301e4d3f5a326b47b6
Shivam Vats <shivamvats.iitkgp@gmail.com>
2017-12-11 13:13:50 +0530

    Use absolute path and create separate jobs to handle first time startup.

--- routinizer/executor.py
+++ routinizer/executor.py
@@ -1,30 +1,51 @@
 import feedparser
 import pickle as pk
 import subprocess
+from datetime import date
+import os
+
+REL_READ_FILE = "../data/read_index"
+REL_SRC_FILE = "../data/paulgraham_essays.rss"
+dir = os.path.dirname(__file__)
 
 def init():
     pass
     #setReadIndex(0)
 
+def incrementIndex():
+    index, date = getReadIndex()
+    if index is None:
+        setReadIndex(0)
+    else:
+        setReadIndex(index+1)
+
 def setReadIndex(index):
-    with open("../data/read_index", 'w') as f:
-        pk.dump(index, f)
+    with open(os.path.join(dir, REL_READ_FILE), 'w') as f:
+        pk.dump((index, date.today()), f)
+
+def getReadIndex():
+    try:
+        with open(os.path.join(dir, REL_READ_FILE), 'r') as f:
+            index, date  = pk.load(f)
+        return (int(index), date)
+    except IOError:
+        # Return the first index.
+        return (None, None)
+
 
 def loadData(linkLocation):
     data = feedparser.parse(linkLocation)
-    read_index = int( pk.load( open("../data/read_index") ) )
-    return (data, read_index)
-
-def getNextLinkAndUpdateIndex(data, index):
-    index += 1
-    newLink = data.entries[index]['link']
-    setReadIndex(index)
-
-    return newLink
+    readIndex, readDate = getReadIndex()
+    if readIndex is None:
+        index = 0
+    else:
+        index = (readIndex + 1) % len(data.entries)
+    return (data, index)
 
 def executeRoutine():
-    data, index = loadData("../data/paulgraham_essays.rss")
-    link = getNextLinkAndUpdateIndex( data, index )
+    data, index = loadData(os.path.join(dir, REL_SRC_FILE))
+    link = data.entries[index]['link']
+    incrementIndex()
 
     print(link)
     # Open the link in a browser.
--- routinizer/scheduler.py
+++ routinizer/scheduler.py
@@ -1,14 +1,28 @@
 import schedule
 import time
-from executor import executeRoutine, init
+from executor import executeRoutine, init, getReadIndex, incrementIndex
+from datetime import date
 
 
-def main():
+def scheduleDaily():
     init()
-    schedule.every().day.at("10:18").do(executeRoutine)
+    schedule.every().day.at("9:00").do(executeRoutine)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-main()
+def scheduleNow():
+    executeRoutine()
+    incrementIndex()
+    scheduleDaily()
+
+def job():
+    todayDate = date.today()
+    _, lastReadDate = getReadIndex()
+    if lastReadDate is None or lastReadDate < todayDate:
+        scheduleNow()
+    else:
+        scheduleDaily()
+
+job()
